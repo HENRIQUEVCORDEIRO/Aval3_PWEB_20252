9dd1f3f27818e928747ce2e547420d85890cd984
Henrique Cordeiro <henrique.verascordeiro@gmail.com>
2025-11-27 17:40:54 -0300

    initializing javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+//fetch("https://restcountries.com/v3.1/all")
+//  .then((res) => res.json())
+//  .then((data) => {
+//    data.forEach((country) => {
+//      console.log(country);
+//    });
+//  });
+
+const countryCard = document.createElement("a");
+
+countryCard.classList.add("countryCard");
+
+console.log(countryCard);
