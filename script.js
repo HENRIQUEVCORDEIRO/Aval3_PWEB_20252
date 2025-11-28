17fa61f920a6a0326de81d0c77893e086007e623
Henrique Cordeiro <henrique.verascordeiro@gmail.com>
2025-11-27 22:25:07 -0300

    establishing communication with the API

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-//fetch("https://restcountries.com/v3.1/all")
-//  .then((res) => res.json())
-//  .then((data) => {
-//    data.forEach((country) => {
-//      console.log(country);
-//    });
-//  });
+const countriesContainer = document.querySelector(".countries-container");
 
-const countryCard = document.createElement("a");
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      console.log(country);
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("countryCard");
+      countryCard.innerHTML = `
+            <img src="https://flagcdn.com/de.svg" alt="flag">
+            <div class="card-content">
+                <h3 class="card-title">Alemanha</h3>
+                <p><b>Populacao:</b>83,491,249</p>
+                <p><b>Continente:</b>Europa</p>
+                <p><b>Capital:</b>Berlim</p>
+            </div>
+        `;
 
-countryCard.classList.add("countryCard");
-
-console.log(countryCard);
+      countriesContainer.append(countryCard);
+    });
+  });
