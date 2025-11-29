70035b32651ac5dc3d8da7749b9363703aaedd02
Henrique Cordeiro <henrique.verascordeiro@gmail.com>
2025-11-29 17:04:40 -0300

    refining info and communication with the API at the details page

--- details.js
+++ details.js
@@ -1,14 +1,53 @@
-const countryURLName = new URLSearchParams(location.search).get("name");
+const countryName = new URLSearchParams(location.search).get("name");
 const flagImg = document.querySelector(".country-details img");
 const countryH1Name = document.querySelector(".country-details h1");
+const nativeName = document.querySelector(".native-name");
+const population = document.querySelector(".population");
+const region = document.querySelector(".region");
+const subRegion = document.querySelector(".sub-region");
+const capital = document.querySelector(".capital");
+const domain = document.querySelector(".domain");
+const currencies = document.querySelector(".currencies");
+const languages = document.querySelector(".languages");
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
   .then((res) => res.json())
-  .then((country) => {
-    console.log(country.name.nativeName);
+  .then(([country]) => {
+    //console.log(Object.values(country.name.nativeName)[0].common);
     flagImg.src = country.flags.svg;
     countryH1Name.innerText = country.name.common;
-  });
+    population.innerText = country.population.toLocaleString("pt-BR");
+    region.innerText = country.region;
+    subRegion.innerText = country.subregion;
+    capital.innerText = country.capital?.[0];
+    domain.innerText = country.tld.join(", ");
 
-//Continuar a parte do nome. Ainda testando pra ver como as requisições
-//estão sendo feitas pelo console, pra garantir que vai dar tudo certo.
+    if (country.capital) {
+      capital.innerText = country.capital?.[0];
+    }
+
+    if (country.subregion) {
+      subRegion.innerText = country.subregion;
+    }
+
+    console.log(country);
+    if (country.name.nativeName) {
+      nativeName.innerText = Object.values(country.name.nativeName)[0].common;
+    } else {
+      nativeName.innerText = country.name.common;
+    }
+
+    if (country.currencies) {
+      currencies.innerText = Object.values(country.currencies)
+        .map((currency) => currency.name)
+        .join(", ");
+    }
+
+    //if (country.capital) {
+    //  console.log(country.capital.join(", "));
+    //}
+
+    if (country.languages) {
+      languages.innerText = Object.values(country.languages).join(", ");
+    }
+  });
--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const countriesContainer = document.querySelector(".container-countries");
 
-
-fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital")
+fetch(
+  "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
+)
   .then((res) => res.json())
   .then((data) => {
-    // console.log(data); 
+    // console.log(data);
 
     data.forEach((country) => {
-      console.log(country);
+      //console.log(country)
+
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
-      countryCard.href = `details.html?name=${country.name.common}`; 
+      countryCard.href = `details.html?name=${country.name.common}`;
 
       countryCard.innerHTML = `
-            <img src="${country.flags.svg}" alt="Bandeira de ${country.name.common}">
+            <img src="${country.flags.svg}" alt="Bandeira de ${
+        country.name.common
+      }">
             <div class="card-content">
                 <h3 class="card-title">${country.name.common}</h3>
-                <p><b>População: </b>${country.population.toLocaleString('pt-BR')}</p>
+                <p><b>População: </b>${country.population.toLocaleString(
+                  "pt-BR"
+                )}</p>
                 <p><b>Continente: </b>${country.region}</p>
-                <p><b>Capital: </b>${country.capital?.[0] || 'Unavailable'}</p>
+                <p><b>Capital: </b>${country.capital?.[0] || "Unavailable"}</p>
             </div>
         `;
 
       countriesContainer.append(countryCard);
     });
-  })
-;
+  });
