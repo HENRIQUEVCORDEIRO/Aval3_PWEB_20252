de34c7abc686320220a98753d2f9d78fe10ce23d
Erick-VP <erickvscletal31@gmail.com>
2025-11-30 12:44:30 -0300

    Function render added

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const countriesContainer = document.querySelector(".container-countries");
 
+
+
+let allCountries = [];
+
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
 )
   .then((res) => res.json())
   .then((data) => {
+    allCountries = data;
+    renderCountries(allCountries);
     // console.log(data);
 
-    data.forEach((country) => {
-      //console.log(country)
+    // data.forEach((country) => {
+    //   //console.log(country)
 
+    //   const countryCard = document.createElement("a");
+    //   countryCard.classList.add("country-card");
+    //   countryCard.href = `details.html?name=${country.name.common}`;
+
+    //   countryCard.innerHTML = `
+    //         <img src="${country.flags.svg}" alt="Bandeira de ${
+    //     country.name.common
+    //   }">
+    //         <div class="card-content">
+    //             <h3 class="card-title">${country.name.common}</h3>
+    //             <p><b>População: </b>${country.population.toLocaleString(
+    //               "pt-BR"
+    //             )}</p>
+    //             <p><b>Continente: </b>${country.region}</p>
+    //             <p><b>Capital: </b>${country.capital?.[0] || "Unavailable"}</p>
+    //         </div>
+    //     `;
+
+    //   countriesContainer.append(countryCard);
+    // });
+  });
+
+  function renderCountries( countries ) {
+    countries.forEach((country) => {
+      //console.log(country)
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
       countryCard.href = `details.html?name=${country.name.common}`;
@@ -24,10 +55,10 @@ fetch(
                   "pt-BR"
                 )}</p>
                 <p><b>Continente: </b>${country.region}</p>
-                <p><b>Capital: </b>${country.capital?.[0] || "Unavailable"}</p>
+                <p><b>Capital: </b>${country.capital?.[0] || "Indisponível"}</p>
             </div>
         `;
-
       countriesContainer.append(countryCard);
     });
-  });
+  }
+
