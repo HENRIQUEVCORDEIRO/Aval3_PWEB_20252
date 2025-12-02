87f279cbfc33edd812657349cdf28d5c4afb73a6
Henrique Cordeiro <henrique.verascordeiro@gmail.com>
2025-12-01 22:23:18 -0300

    Search bar now functioning

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const countriesContainer = document.querySelector(".container-countries");
 const regionCheckboxes = document.querySelectorAll("input[name='region']");
+const searchInput = document.querySelector(".search-container input");
 
-
-let allCountries = [];
+let allCountries;
 
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
@@ -10,23 +10,22 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     allCountries = data;
-    renderCountries(allCountries);
-
+    renderCountries(data);
   });
 
-function renderCountries( countries ) {
+function renderCountries(countries) {
   countriesContainer.innerHTML = "";
 
   countries.forEach((country) => {
-      //console.log(country)
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `details.html?name=${country.name.common}`;
+    //console.log(country)
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `details.html?name=${country.name.common}`;
 
-      countryCard.innerHTML = `
+    countryCard.innerHTML = `
             <img src="${country.flags.svg}" alt="Bandeira de ${
-        country.name.common
-      }">
+      country.name.common
+    }">
             <div class="card-content">
                 <h3 class="card-title">${country.name.common}</h3>
                 <p><b>População: </b>${country.population.toLocaleString(
@@ -36,12 +35,12 @@ function renderCountries( countries ) {
                 <p><b>Capital: </b>${country.capital?.[0] || "Indisponível"}</p>
             </div>
         `;
-      countriesContainer.append(countryCard);
-    });
-  }
+    countriesContainer.append(countryCard);
+  });
+}
 
 regionCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", (applyFilters));
+  checkbox.addEventListener("change", applyFilters);
 });
 
 function applyFilters() {
@@ -49,9 +48,20 @@ function applyFilters() {
     .filter((checkbox) => checkbox.checked)
     .map((checkbox) => checkbox.value);
 
-  const filteredCountries = allCountries.filter((country) => { //isso aqui tá muito feio Meu Deus
+  const filteredCountries = allCountries.filter((country) => {
+    //isso aqui tá muito feio Meu Deus
     if (selectedRegions.length === 0) return true;
     return selectedRegions.includes(country.region);
   });
   renderCountries(filteredCountries);
 }
+
+searchInput.addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  //console.log(allCountries)
+  const searchedCountries = allCountries.filter((country) =>
+    country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  console.log(searchedCountries);
+  renderCountries(searchedCountries);
+});
